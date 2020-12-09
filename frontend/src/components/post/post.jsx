61780be6c1e5e39066c1bf25bcec1a73fcf58c67
unknown <skekdnl12@gmail.com>
2020-12-09 22:24:11 +0900

    2020.12.09 back단과 통신

--- frontend/src/components/post/post.jsx
+++ frontend/src/components/post/post.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import ShowPost from './showpost/showPost';
-
-const Post = (props) => {
-    
-       return (
-           <>
-            <div>
-                <h1>JavaScript</h1>
-                {/* {props.items.map((item) => <ShowPost item={item}/>)} */}
-                
-            </div>
-            <div>
-
-            </div>
-           </>
-       );
-    };
+import React, { useEffect, useState } from 'react';
+import ShowPosts from './showpost/showPosts';
+// arrow function 사용시 함수가 문구 혹은 덩어리로 하나라면 소괄호가능 그렇지 않으면 중괄호 return필수
+const Post = (props) => { 
+    const [posts,setPosts] = useState([])
+    useEffect(()=>{
+        const requestOptions ={
+            method : "GET"
+        }
+        fetch("http://localhost:8000/post/",requestOptions)
+        .then(response => response.json())
+        .then(result => setPosts(result.posts))
+        .catch(error => alert('불러오기 실패',error))
+    },[])
+    return (
+        <>
+            <ShowPosts posts={posts}/>
+        </>
+    );
+};
 
 export default Post;
