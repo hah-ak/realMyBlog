d3e3cf6be91060a565d5df2a49e8d78076bfdb18
unknown <skekdnl12@gmail.com>
2020-12-09 22:19:23 +0900

    2020.12.09 postpage url 추가

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
+    path('',views.post,name='post'),
     path('javascript/', views.javascript, name='javascript'),
 ]
